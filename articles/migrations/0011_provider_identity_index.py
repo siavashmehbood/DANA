1a3c28b0ad5c7c26e0989219a0d06acbf4f4a322
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-21 20:27:38 +0330

    fix(migrations): align article index state

--- articles/migrations/0011_provider_identity_index.py
+++ articles/migrations/0011_provider_identity_index.py
@@ -2,4 +2,4 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies=[('articles','0010_unique_article_source_name')]
-    operations=[migrations.AddIndex(model_name='article',index=models.Index(fields=['source_provider','external_id'],name='articles_ar_source__2c663f_idx'))]
+    operations=[migrations.AddIndex(model_name='article',index=models.Index(fields=['source_provider','external_id'],name='articles_ar_source__dcb879_idx'))]
