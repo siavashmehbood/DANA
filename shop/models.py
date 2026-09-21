753b9a73b4b0e6501da2a9755c7fe3b13e48a3ee
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-21 21:57:19 +0330

    feat(subscription): support featured membership plans

--- shop/models.py
+++ shop/models.py
@@ -110,6 +110,7 @@ class SubscriptionPlan(models.Model):
     duration_days = models.PositiveIntegerField(default=30, validators=[MinValueValidator(1)])
     active = models.BooleanField(default=True)
     grants_catalog_access = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
