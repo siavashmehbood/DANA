3fadab7cdedd68fef01cc308d633e98805eb92e7
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-21 21:21:56 +0330

    fix(migrations): align analytics index names

--- analytics/migrations/0002_event_indexes.py
+++ analytics/migrations/0002_event_indexes.py
@@ -2,6 +2,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies=[('analytics','0001_initial')]
     operations=[
-      migrations.AddIndex(model_name='event',index=models.Index(fields=['name','created_at'],name='analytics_e_name_95d506_idx')),
-      migrations.AddIndex(model_name='event',index=models.Index(fields=['user','created_at'],name='analytics_e_user_id_9434cf_idx')),
+      migrations.AddIndex(model_name='event',index=models.Index(fields=['name','created_at'],name='analytics_e_name_bf1d30_idx')),
+      migrations.AddIndex(model_name='event',index=models.Index(fields=['user','created_at'],name='analytics_e_user_id_83f61f_idx')),
     ]
