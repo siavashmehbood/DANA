1d4cc224f4dd8fe0dd362db5742fc26085fa720a
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-21 20:20:27 +0330

    feat(media): route protected book files

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,3 @@
 from django.urls import path
-from .views import listing,detail
-urlpatterns=[path('',listing,name='books'),path('<slug:slug>/',detail,name='book_detail')]
+from .views import listing,detail,secure_file
+urlpatterns=[path('',listing,name='books'),path('<int:pk>/file/<str:kind>/',secure_file,name='book_secure_file'),path('<slug:slug>/',detail,name='book_detail')]
