3c4cf7eeeb1fd810ae4ecfe239d210530a131020
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-21 20:22:27 +0330

    fix(migrations): match Django entitlement index name

--- shop/migrations/0004_coupon_validation_entitlement_index.py
+++ shop/migrations/0004_coupon_validation_entitlement_index.py
@@ -5,5 +5,5 @@ class Migration(migrations.Migration):
     dependencies=[('shop','0003_entitlement_source')]
     operations=[
         migrations.AlterField(model_name='coupon',name='percent',field=models.PositiveSmallIntegerField(default=0,validators=[django.core.validators.MaxValueValidator(100)])),
-        migrations.AddIndex(model_name='entitlement',index=models.Index(fields=['user','expires_at'],name='shop_entitl_user_id_exp_idx')),
+        migrations.AddIndex(model_name='entitlement',index=models.Index(fields=['user','expires_at'],name='shop_entitl_user_id_69e5c2_idx')),
     ]
