6be8c4b4dfb568a0ee0225f594eba80ffa09f6e2
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-21 22:32:16 +0330

    chore(books): remove unused URL import

--- books/views.py
+++ books/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,get_object_or_404
 from django.db.models import Q, Avg, Count
 from django.core.paginator import Paginator
 from django.http import FileResponse, HttpResponseForbidden
-from django.urls import reverse
 from django.utils import timezone
 from shop.models import Entitlement
 from analytics.models import Event
