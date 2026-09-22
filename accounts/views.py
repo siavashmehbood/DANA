d7540ac086cfcb5931aaf51b8440a4a1948c9416
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 11:32:48 +0330

    fix(library): allow expired subscribers to rediscover catalog

--- accounts/views.py
+++ accounts/views.py
@@ -202,7 +202,7 @@ def library(request):
     preferred_categories={book.category_id for book in books if book.category_id}
     excluded_recommendation_ids=set(book_ids)
     excluded_recommendation_ids.update(Entitlement.objects.filter(user=user).values_list('book_id',flat=True))
-    if Subscription.objects.filter(user=user).exists():
+    if active_subscription:
         excluded_recommendation_ids.update(Book.objects.filter(subscription_included=True).values_list('id',flat=True))
     if not preferred_categories:
         preferred_categories=set(ReadingProgress.objects.filter(user=user,book__category__isnull=False).exclude(book_id__in=excluded_recommendation_ids).values_list('book__category_id',flat=True)[:20])
