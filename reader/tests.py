e5fad9cdb63766ba09424bbf508f19af29d97de4
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 06:58:14 +0330

    fix(tests): initialize reader interaction fixtures

--- reader/tests.py
+++ reader/tests.py
@@ -151,6 +151,12 @@ class StudyFlowTests(TestCase):
 
 
 class SubscriptionReaderAccessTests(TestCase):
+    def setUp(self):
+        self.user=User.objects.create_user(username='subscription-reader',password='pass12345')
+        author=Author.objects.create(name='Subscription Test Author')
+        self.book=Book.objects.create(name='Subscription Test Book',slug='subscription-test-book',author=author,status='published',visibility='public')
+        self.client.force_login(self.user)
+
     def test_active_catalog_subscription_can_open_private_reader(self):
         user=User.objects.create_user(username='sub-reader',password='pass12345')
         author=Author.objects.create(name='Sub Reader Author')
