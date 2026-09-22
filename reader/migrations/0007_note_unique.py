cc23115baaa3ed7dc8af35821c76e96404880c76
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 07:04:03 +0330

    fix(migrations): align note constraint migration state

--- reader/migrations/0007_note_unique.py
+++ reader/migrations/0007_note_unique.py
@@ -3,5 +3,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies=[('reader','0006_highlight')]
     operations=[
+        migrations.AlterModelOptions(name='note',options={}),
         migrations.AddConstraint(model_name='note',constraint=models.UniqueConstraint(fields=('user','book','page','text'),name='unique_reader_note')),
     ]
