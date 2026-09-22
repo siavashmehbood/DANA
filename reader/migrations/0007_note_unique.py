4f709f537fda2f5f7cb6c8d24415609e0d145626
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 07:06:01 +0330

    chore(migrations): refresh reader migration graph

--- reader/migrations/0007_note_unique.py
+++ reader/migrations/0007_note_unique.py
@@ -2,6 +2,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies=[('reader','0006_highlight')]
+    replaces=[]
     operations=[
         migrations.AddConstraint(model_name='note',constraint=models.UniqueConstraint(fields=('user','book','page','text'),name='unique_reader_note')),
     ]
