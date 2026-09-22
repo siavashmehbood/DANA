3301c5c1daa663e8a4846eafd032e1e9113a0e23
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 13:47:03 +0330

    Make reader activity admin operational and read-only

--- reader/admin.py
+++ reader/admin.py
@@ -35,7 +35,37 @@ class ReadingGoalAdmin(ModelAdmin):
     list_display=('user','weekly_minutes','weekly_books','updated_at')
     search_fields=('user__username','user__phone')
 
-admin.site.register([ReadingProgress, Bookmark, Highlight, Note])
+@admin.register(ReadingProgress)
+class ReadingProgressAdmin(ModelAdmin):
+    list_display=('user','book','progress','current_page','current_chapter','updated_at')
+    list_filter=('updated_at',)
+    search_fields=('user__username','user__phone','book__name')
+    readonly_fields=('user','book','progress','current_page','current_chapter','seconds','audio_seconds','updated_at')
+    date_hierarchy='updated_at'
+    def has_add_permission(self, request): return False
+    def has_delete_permission(self, request, obj=None): return False
+
+@admin.register(Bookmark)
+class BookmarkAdmin(ModelAdmin):
+    list_display=('user','book','page','title','created_at')
+    search_fields=('user__username','user__phone','book__name','title')
+    readonly_fields=('user','book','page','title','created_at')
+    def has_add_permission(self, request): return False
+
+@admin.register(Highlight)
+class HighlightAdmin(ModelAdmin):
+    list_display=('user','book','page','color','created_at')
+    list_filter=('color','created_at')
+    search_fields=('user__username','user__phone','book__name','text')
+    readonly_fields=('user','book','page','text','color','created_at')
+    def has_add_permission(self, request): return False
+
+@admin.register(Note)
+class NoteAdmin(ModelAdmin):
+    list_display=('user','book','page','created_at')
+    search_fields=('user__username','user__phone','book__name','text')
+    readonly_fields=('user','book','page','text','created_at')
+    def has_add_permission(self, request): return False
 
 @admin.register(AudioProgress)
 class AudioProgressAdmin(ModelAdmin):
