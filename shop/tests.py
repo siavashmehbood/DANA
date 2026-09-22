f1e9991b69136d48b5df056081deabcb57ee339b
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 06:46:55 +0330

    test(subscription): exclude disabled plans from current membership

--- shop/tests.py
+++ shop/tests.py
@@ -207,3 +207,11 @@ class ShopFlowTests(TestCase):
         self.client.get(reverse('subscriptions'))
         sub.refresh_from_db()
         self.assertEqual(sub.status,'expired')
+
+
+    def test_disabled_plan_is_not_reported_as_current_subscription(self):
+        plan=SubscriptionPlan.objects.create(name='Disabled Current',slug='disabled-current',price=0,duration_days=7,active=False)
+        Subscription.objects.create(user=self.user,plan=plan,starts_at=timezone.now()-timedelta(days=1),expires_at=timezone.now()+timedelta(days=2))
+        self.client.login(username='buyer',password='pass12345')
+        response=self.client.get(reverse('subscriptions'))
+        self.assertIsNone(response.context['current_subscription'])
