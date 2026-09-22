72666b0061402feea7f25177916c20ac8c6eae18
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 06:59:47 +0330

    feat(migrations): add reader highlights

---
+++ reader/migrations/0006_highlight.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+class Migration(migrations.Migration):
+    dependencies=[('accounts','0001_initial'),('books','0006_book_metadata')]
+    operations=[
+        migrations.CreateModel(
+            name='Highlight',
+            fields=[
+                ('id',models.BigAutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')),
+                ('page',models.PositiveIntegerField()),
+                ('text',models.TextField()),
+                ('created_at',models.DateTimeField(auto_now_add=True)),
+                ('book',models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,to='books.book')),
+                ('user',models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,to='accounts.user')),
+            ],
+        ),
+        migrations.AddIndex(model_name='highlight',index=models.Index(fields=['user','book','page'],name='reader_highlight_lookup_idx')),
+    ]
