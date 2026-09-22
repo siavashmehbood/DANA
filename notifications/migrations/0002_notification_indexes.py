dbc98db7c70f333007319c134d357fefe2cc72b4
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 07:22:54 +0330

    fix(migrations): use generated notification migration name

--- notifications/migrations/0002_notification_indexes.py
+++
@@ -1,8 +0,0 @@
-from django.db import migrations, models
-
-class Migration(migrations.Migration):
-    dependencies=[('notifications','0001_initial')]
-    operations=[
-        migrations.AddIndex(model_name='notification',index=models.Index(fields=['user','-created_at'],name='notify_user_created_idx')),
-        migrations.AddIndex(model_name='notification',index=models.Index(fields=['user','read_at'],name='notify_user_read_idx')),
-    ]
