b2b3c08d4059ca10cf1240b49de77f9205843b28
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 09:28:39 +0330

    feat(migrations): add analytics session attribution

---
+++ analytics/migrations/0003_event_session_key.py
@@ -0,0 +1,11 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+    dependencies=[('analytics','0002_event_indexes')]
+    operations=[
+        migrations.AddField(
+            model_name='event',
+            name='session_key',
+            field=models.CharField(blank=True,db_index=True,max_length=64),
+        ),
+    ]
