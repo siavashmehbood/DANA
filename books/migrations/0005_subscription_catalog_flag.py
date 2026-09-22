e8d4efba8f38f72aa9b661eea0d993638c0d2de7
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 07:48:34 +0330

    feat(migrations): add subscription catalog eligibility

---
+++ books/migrations/0005_subscription_catalog_flag.py
@@ -0,0 +1,13 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [('books', '0004_validate_prices')]
+
+    operations = [
+        migrations.AddField(
+            model_name='book',
+            name='subscription_included',
+            field=models.BooleanField(default=False),
+        ),
+    ]
