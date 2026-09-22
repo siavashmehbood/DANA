f07fdd4cf2ec692ff692ff0a17a5badd97c8d746
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 09:32:19 +0330

    fix(analytics): mark session attribution immutable in forms

--- analytics/models.py
+++ analytics/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     name=models.CharField(max_length=80)
     value=models.DecimalField(max_digits=14,decimal_places=2,default=0)
     metadata=models.JSONField(default=dict,blank=True)
-    session_key=models.CharField(max_length=64,blank=True,db_index=True)
+    session_key=models.CharField(max_length=64,blank=True,db_index=True,editable=False)
     created_at=models.DateTimeField(auto_now_add=True)
 
     class Meta:
