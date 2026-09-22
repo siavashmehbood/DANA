b5c269c85cc1f4c48043c48310e2885012d74cbd
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 07:45:55 +0330

    fix(privacy): disable support ticket caching

--- support/views.py
+++ support/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator
+from django.views.decorators.cache import never_cache
 from .models import Ticket, TicketMessage
 
 @login_required
+@never_cache
 def tickets(request):
     if request.method == 'POST':
         subject = request.POST.get('subject','').strip()[:250]
