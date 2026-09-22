2503a2725760a70789e4ea69ca5bed05bfe3c62d
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-22 08:15:39 +0330

    security(media): constrain protected PDF rendering

--- core/views.py
+++ core/views.py
@@ -65,6 +65,7 @@ def protected_book_pdf(request, pk):
     response = FileResponse(book.pdf.open('rb'), content_type='application/pdf')
     response['Content-Disposition'] = f'inline; filename="book-{book.pk}.pdf"'
     response['X-Content-Type-Options'] = 'nosniff'
+    response['Content-Security-Policy'] = "default-src 'none'; frame-ancestors 'self'; sandbox allow-scripts allow-same-origin"
     response['Cache-Control'] = 'private, no-store'
     response['Content-Length'] = str(size)
     return response
