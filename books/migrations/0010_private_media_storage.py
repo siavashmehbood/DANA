71174619cebefa9ceefa3e9a619b066c18fa531f
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-23 13:22:13 +0330

    fix private media migration dependency

--- books/migrations/0010_private_media_storage.py
+++ books/migrations/0010_private_media_storage.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('books', '0009_book_featured'),
+        ('books', '0008_book_featured'),
     ]
 
     operations = [
