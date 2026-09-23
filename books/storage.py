093d13828c40a15b00fd27e145673b24e74cc9e1
siavashmehbood <siavashmehboodi2@gmail.com>
2026-09-23 13:21:18 +0330

    make private storage migration-stable

--- books/storage.py
+++ books/storage.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
 
 class PrivateMediaStorage(FileSystemStorage):
     """Filesystem storage for protected assets with no client-facing URL."""
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.PRIVATE_MEDIA_ROOT
+        super().__init__(*args, **kwargs)
 
     @property
     def base_url(self):
